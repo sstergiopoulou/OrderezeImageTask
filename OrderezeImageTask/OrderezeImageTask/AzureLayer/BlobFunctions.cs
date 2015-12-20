8af4164a9e24a6f6b44585ed701671fcbef4de8f
sstergiopoulou <sstergiopoulou@yahoo.gr>
2015-12-20 21:29:53 +0200

    1) Add BlobFunctions.cs in folder AzureLayer (etc. upload file to Blob, delete blob file, get blob files ...) 2) Add ImageDBFunctions.cs (insert and delete image from database, get images from database) 3) Add ImageService.cs implementing IImageService interface 4) Add connection string in web.config

---
+++ OrderezeImageTask/OrderezeImageTask/AzureLayer/BlobFunctions.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.IO;
+using System.Configuration;
+using System.Collections.Generic;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using OrderezeImageTask.Models;
+
+
+namespace OrderezeImageTask.AzureLayer
+{
+    public class BlobFunctions
+    {
+
+        public CloudBlobClient blobClientConnect(string connstring)
+        {
+            // Retrieve storage account from connection string.
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(ConfigurationManager.AppSettings[connstring].ToString());
+            // Create and return the blob client.
+            return storageAccount.CreateCloudBlobClient();
+        }
+
+        public CloudBlobContainer blobGetContainerRef(CloudBlobClient blobclient, string containerName)
+        {
+            // Retrieve reference to a previously created container.
+            CloudBlobContainer container = blobclient.GetContainerReference(containerName);
+            // Create the container if it doesn't already exist.
+            container.CreateIfNotExists();
+            // By default, the new container is private, so we set set the container to be public
+            container.SetPermissions(
+                new BlobContainerPermissions
+                {
+                    PublicAccess = BlobContainerPublicAccessType.Container
+                });
+            return container;
+        }
+
+        public CloudBlockBlob blobGetBlobRef(CloudBlobContainer container, string blobname)
+        {
+            // Retrieve reference to a blob named
+            return container.GetBlockBlobReference(blobname);
+        }
+
+        public void setPublicPermissions(CloudBlobContainer container, BlobContainerPublicAccessType Level)
+        {
+            container.SetPermissions(
+                new BlobContainerPermissions
+                {
+                    PublicAccess = Level
+                });
+        }
+
+        public void addBlobMetadata(CloudBlockBlob blob, string metadataKey, string metadataValue)
+        {
+            blob.Metadata.Add(metadataKey, metadataValue);
+        }
+
+        public string uploadFileToBlob(Image imageforupload)
+        {
+            var blobcontainer = blobGetContainerRef(blobClientConnect("StorageConnectionString"), "imagecontainer");
+            var blob = blobGetBlobRef(blobcontainer, Guid.NewGuid().ToString() + Path.GetExtension(imageforupload.ImagePath));
+            // Create or overwrite the blob with contents from a local file.
+            using (var fileStream = System.IO.File.OpenRead(imageforupload.ImagePath))
+            {
+                blob.UploadFromStream(fileStream);
+            }
+
+            blob.Properties.ContentType = System.Web.MimeMapping.GetMimeMapping(imageforupload.ImagePath);
+            blob.SetProperties();
+            // Return a URI for viewing the photo
+            return blob.Uri.ToString();
+        }
+
+        public List<Image> getBlobFiles()
+        {
+            List<Image> imageList = new List<Image>();
+            var blobcontainer = blobGetContainerRef(blobClientConnect("StorageConnectionString"), "imagecontainer");
+            // Loop over items within the container and get image list
+            foreach (var blobItem in blobcontainer.ListBlobs())
+            {
+                var aBlob = blobGetBlobRef(blobcontainer, blobItem.Uri.AbsoluteUri);
+                aBlob.FetchAttributes();
+
+                imageList.Add(new Image()
+                {
+                    Id = int.Parse(aBlob.Metadata["id"]),
+                    Name = aBlob.Metadata["name"],
+                    Description = aBlob.Metadata["description"],
+                    ImagePath = aBlob.Uri.AbsoluteUri
+                });
+            }
+            return imageList;
+        }
+
+        public void deleteBlobFile(string bloburi)
+        {
+            var blobcontainer = blobGetContainerRef(blobClientConnect("StorageConnectionString"), "imagecontainer");
+            var blob = blobGetBlobRef(blobcontainer, Path.GetFileName(bloburi));
+            // Delete the blob if exists.
+            blob.DeleteIfExists();
+        }
+    }
+}
---
+++ OrderezeImageTask/OrderezeImageTask/DataAccessLayer/ImageService.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using OrderezeImageTask.Models;
+using OrderezeImageTask.AzureLayer;
+
+namespace OrderezeImageTask.DataAccessLayer
+{
+    public class ImageService : IImagesService
+    {
+        BlobFunctions    blf = new BlobFunctions();
+        ImageDBFunctions dbf = new ImageDBFunctions();
+
+        /// <summary>
+        /// Returns all images 
+        /// </summary>
+        public List<Image> GetImages()
+        {
+            return dbf.getImagesFromDB();
+        }
+
+        /// <summary>
+        /// Adds the supplied <paramref name="image"/> to the system and returns the Id.
+        /// Store the Image in the blob storage.
+        /// </summary>
+        public int AddNewImage(Image image)
+        {
+            image.ImagePath = blf.uploadFileToBlob(image);
+            image.Id = dbf.insertImageToDb(image);
+
+            return image.Id;
+        }
+
+        /// <summary>
+        /// Deletes the Image with the supplied <paramref name="id"/> from the system 
+        /// and deletes the file from the blob storage as well.
+        /// </summary>
+        public void DeleteImage(int id)
+        {
+            blf.deleteBlobFile(dbf.deleteImagefromDB(id));
+        }
+    }
+}
